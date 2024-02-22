7376b34f615a8d77cacda7289409a8cf7215943b
Sarthak dhoke <sarthakdhoke132@gmail.com>
2024-02-23 02:40:51 +0530

    added Typecasting.cpp file

--- Lecture-1/Average.cpp
+++ Lecture-1/Average.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+
 using namespace std;
 
-int main(){
-    int a,b,c;
+int main()
+
+{
+    int a, b, c;
 
     cout << "Enter the value of a, b and c : " << endl;
     cin >> a >> b >> c;
 
-    float avg = (a+b+c)/3;
+    float avg = (a + b + c) / 3;
 
     cout << "The average is : " << avg << endl;
 
---
+++ Lecture-2 (data-types)/Typecasting.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+//     int z = 'a';
+//     cout << z << endl;
+//     return 0;
+// }
+int main(){
+    char z = 98;
+    cout << z << endl;
+    return 0;
+}
