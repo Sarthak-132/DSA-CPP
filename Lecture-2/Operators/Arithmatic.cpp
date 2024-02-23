39da658cf47b8e86702428d0a65bbf548e682b97
Sarthak dhoke <sarthakdhoke132@gmail.com>
2024-02-24 00:56:01 +0530

    added folder 2 for operators

---
+++ Lecture-2/Operators/Arithmatic.cpp
@@ -0,0 +1,20 @@
+//1. Arithmatic Operations
+(+, -, *, /, %)
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  int y = 3;
+  cout << x + y << endl;    
+  cout << x - y << endl;
+  cout << x * y << endl;
+  cout << x % y << endl;
+  cout << x / y << endl;
+//   cout << ++x << endl;
+//   cout << --x << endl;
+  return 0;
+}
+
+
+
---
+++ Lecture-2/Operators/Assignment.cpp
@@ -0,0 +1,16 @@
+// Assignment Operators
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 10;
+  x += 5;
+  // x -= 5;
+  // x *= 5;
+  // x /= 5;
+  // x %= 5;
+  // x++;
+  // x--;
+  cout << x;
+  return 0;
+}
