5486069832c18f59196e3461c9617becbfa65227
unknown <sarthakdhoke132@gmail.com>
2024-09-26 17:23:55 +0530

    learning patterns of square

--- Lecture-4/Triangle/pattern-5.cpp
+++ Lecture-4/Triangle/pattern-5.cpp
@@ -3,3 +3,28 @@
 // 3 4 5
 // 4 5 6 7
 
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    
+    int n;
+
+    cin>> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int value = i;
+
+            for (int j = 1; j <= i; j++)
+            {
+                cout<<value<<" ";
+                value = value + 1;
+            }
+            
+        cout<<endl;
+    }
+    
+    return 0;
+}
