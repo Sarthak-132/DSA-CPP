105ecdbc67d486c6ec8fce7bc21f32857045255f
Sarthak dhoke <sarthakdhoke132@gmail.com>
2024-12-03 15:28:41 +0530

    worked on leetcode question and added some notes

--- lecture-5/Leetcode/Leetcode-191.cpp
+++ lecture-5/Leetcode/Leetcode-191.cpp
@@ -6,14 +6,22 @@
 // suppose int a = 8; it's binary represantation 1000 (so number of 1 bit is = 1)
 // or int b = 5; binary represantation 101 (so number of 1 bit is = 2)
 
-// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+// Given a positive integer n, write a function that returns the number of 
+// set bits
+//  in its binary representation (also known as the Hamming weight).
+
+
 // Example 1:
-// Input: n = 234
-// Output: 15 
-// Explanation: 
-// Product of digits = 2 * 3 * 4 = 24 
-// Sum of digits = 2 + 3 + 4 = 9 
-// Result = 24 - 9 = 15
+
+// Input: n = 11
+
+// Output: 3
+
+// Explanation:
+
+// The input binary string 1011 has a total of three set bits.
+
 
 class Solution {
 public:
